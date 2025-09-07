6976ac00bec196ac6658772abc9c9594b36f493a
theaurence <theaurence@gmail.com>
2025-09-07 15:26:32 +0530

    Deploy: ignore ESLint during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: { ignoreDuringBuilds: true },
+  };
+  export default nextConfig;
